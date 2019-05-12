32f93ba9a5b95daa439b9e3c8a0c55e0c84491ab
vika <victorypa1992@yandex.ru>
2019-05-12 23:24:19 +0300

    logical operators

--- app.js
+++ app.js
@@ -140,40 +140,40 @@ val = str.includes('foo');
 
 //TEMPLATE LITERALS
 
-const name = 'John';
-const age = 31;
-const job = 'Web Developer';
-const city = 'Miami';
-let html;
+// const name = 'John';
+// const age = 31;
+// const job = 'Web Developer';
+// const city = 'Miami';
+// let html;
 
-// Without template strings (es5)
-html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job: '+ job+ ' </li><li>City: '+ city +' </li></ul>';
+// // Without template strings (es5)
+// html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job: '+ job+ ' </li><li>City: '+ city +' </li></ul>';
 
-html = '<ul>' +
-       '<li>Name: ' + name + '</li>' +
-       '<li>Age: ' + age + '</li>' +
-       '<li>Job: ' + job + '</li>' +
-       '<li>City: ' + city + '</li>' +
-       '</ul>';
+// html = '<ul>' +
+//        '<li>Name: ' + name + '</li>' +
+//        '<li>Age: ' + age + '</li>' +
+//        '<li>Job: ' + job + '</li>' +
+//        '<li>City: ' + city + '</li>' +
+//        '</ul>';
 
-function hello(){
-  return 'hello';
-}
+// function hello(){
+//   return 'hello';
+// }
 
 // With template strings (es6)
-html = `
-  <ul>
-    <li>Name: ${name}</li>
-    <li>Age: ${age}</li>
-    <li>Job: ${job}</li>
-    <li>City: ${city}</li>
-    <li>${2 + 2}</li>
-    <li>${hello()}</li>
-    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
-  </ul>
-`;
+// html = `
+//   <ul>
+//     <li>Name: ${name}</li>
+//     <li>Age: ${age}</li>
+//     <li>Job: ${job}</li>
+//     <li>City: ${city}</li>
+//     <li>${2 + 2}</li>
+//     <li>${hello()}</li>
+//     <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+//   </ul>
+// `;
 
-document.body.innerHTML = html;
+// document.body.innerHTML = html;
 
 //ARRAYS & ARRAY METHODS
 
@@ -236,71 +236,165 @@ val = numbers.sort(function(x, y){
 
 //OBJECT LITERALS
 
-const person = {
-  firstName: 'Steve',
-  lastName: 'Smith',
-  age: 27,
-  email: 'steve@aol.com',
-  hobbies: ['music', 'sports'],
-  address: {
-    city: 'Miami',
-    state: 'FL'
-  },
-  getBirthYear: function(){
-    return 2019 - this.age;
-  }
+// const person = {
+//   firstName: 'Steve',
+//   lastName: 'Smith',
+//   age: 27,
+//   email: 'steve@aol.com',
+//   hobbies: ['music', 'sports'],
+//   address: {
+//     city: 'Miami',
+//     state: 'FL'
+//   },
+//   getBirthYear: function(){
+//     return 2019 - this.age;
+//   }
 
-}
+// }
 // let val;
 
-val = person; 
+// val = person; 
 
 //Get specific value
 
-val = person.firstName;
-val = person.age;
-val = person.hobbies[1];
-val = person.address.state;
-val = person.address['city'];
-val = person.getBirthYear();
+// val = person.firstName;
+// val = person.age;
+// val = person.hobbies[1];
+// val = person.address.state;
+// val = person.address['city'];
+// val = person.getBirthYear();
 
-console.log(val);
+// console.log(val);
 
 
-const people = [
-  {name: 'John', age: 30},
-  {name: 'Mike', age: 23},
-  {name: 'Nancy', age: 23}
-];
+// const people = [
+//   {name: 'John', age: 30},
+//   {name: 'Mike', age: 23},
+//   {name: 'Nancy', age: 23}
+// ];
 
-for(let i = 0; i < people.length; i++){
-  console.log(people[i].name);
-}
+// for(let i = 0; i < people.length; i++){
+//   console.log(people[i].name);
+// }
 
 
 //DATES & TIMES
 
 // let val;
 
-const today = new Date();
-let birthday = new Date('9-10-1981 11:25:00');
-birthday = new Date('September 10 1981');
-birthday = new Date('9/10/1981');
+// const today = new Date();
+// let birthday = new Date('9-10-1981 11:25:00');
+// birthday = new Date('September 10 1981');
+// birthday = new Date('9/10/1981');
 
-val = today.getMonth();
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
+// val = today.getMonth();
+// val = today.getDate();
+// val = today.getDay();
+// val = today.getFullYear();
+// val = today.getHours();
+// val = today.getMinutes();
+// val = today.getSeconds();
+// val = today.getMilliseconds();
 
-birthday.setMonth(2);
-birthday.setDate(12);
-birthday.setFullYear(1985);
-birthday.setHours(3);
-birthday.setMinutes(30);
-birthday.setSeconds(25);
+// birthday.setMonth(2);
+// birthday.setDate(12);
+// birthday.setFullYear(1985);
+// birthday.setHours(3);
+// birthday.setMinutes(30);
+// birthday.setSeconds(25);
 
-console.log(birthday);
+// console.log(birthday);
+
+
+
+//IF STATEMENTS & COMPARISON OPERATORS
+
+// const id = 100;
+
+// // EQUAL TO
+// if(id == 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO
+// if(id != 101){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE & TYPE
+// if(id === 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE & TYPE
+// if(id !== 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// Test if undefined
+// if(typeof id !== 'undefined') {
+//   console.log(`The ID is ${id}`);
+// } else {
+//   console.log('NO ID')
+// }
+
+// GREATER OR LESS THAN
+// if(id <= 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+const color = 'yellow';
+
+// if(color === 'red'){
+//   console.log('Color is red');
+// } else if(color === 'blue'){
+//   console.log('Color is blue');
+// } else {
+//   console.log('Color is not red or blue');
+// }
+
+
+// LOGICAL OPERATORS
+
+const name = 'Steve';
+const age = 70;
+
+// AND &&
+// if(age > 0 && age < 12){
+//   console.log(`${name} is a child`);
+// } else if(age >= 13 && age <= 19){
+//   console.log(`${name} is a teenager`);
+// } else {
+//   console.log(`${name} is an adult`);
+// }
+
+// OR ||
+// if(age < 16 || age > 65){
+//   console.log(`${name} can not run in race`);
+// } else {
+//   console.log(`${name} is registered for the race`);
+// }
+
+// TERNARY OPERATOR
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// WITHOUT BRACES
+if(id === 100)
+  console.log('CORRECT');
+else
+  console.log('INCORRECT');
+
+if(id === 100)
+  console.log('CORRECT');
+else
+  console.log('INCORRECT');
