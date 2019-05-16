04aef0b32b4af973e43396bedccfc0c96db21fb9
Victorypa <victorypa1992@yandex.ru>
2019-05-16 23:29:38 +0300

    DOM Selectors for single Elements

--- app.js
+++ app.js
@@ -664,14 +664,13 @@ const c = 3;
 //   console.log(`Loop: ${a}`);
 // }
 
-console.log('Global Scope: ', a, b, c);
+// console.log('Global Scope: ', a, b, c);
 
 
 
 //DOM
-// test
 
-let val;
+// let val;
 
 val = document;
 val = document.all;
@@ -706,8 +705,52 @@ let scripts = document.scripts;
 
 let scriptsArr = Array.from(scripts);
 
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
-});
+// scriptsArr.forEach(function(script) {
+//   console.log(script.getAttribute('src'));
+// });
 
-console.log(val);
+// console.log(val);
+
+// document.getElementById() 
+console.log(document.getElementById('task-title'));
+
+// // Get things from the element
+
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
+
+
+const taskTitle = document.getElementById('task-title');
+
+// Change styling
+
+document.getElementById('task-title').style.background = '#333';
+taskTitle.style.color = '#fff';
+taskTitle.style.padding = '5px';
+// taskTitle.style.display = 'none';
+
+
+// Change content
+
+taskTitle.textContent = 'Task List';
+taskTitle.innerText = 'My Tasks';
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+
+// document.querySelector()
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'yellow';
+document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
+
+
+
