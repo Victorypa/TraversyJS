61fccb2932edafbef144d3a7e9333b7952a7ef04
Victorypa <victorypa1992@yandex.ru>
2019-05-17 15:26:43 +0300

    creating elements

--- app.js
+++ app.js
@@ -868,5 +868,43 @@ val = listItem.previousSibling;
 val = listItem.previousElementSibling;
 
 
-console.log(val);
+// console.log(val);
+
+
+// Create element
+const li = document.createElement('li');
+
+// Add class
+li.className = 'collection-item';
+
+
+//Add id 
+li.id = 'new-item';
+
+//Add attribute
+li. setAttribute('title', 'New Item');
+
+//Create text node and append
+li.appendChild(document.createTextNode('Hello world'));
+
+//Create new link el
+const link = document.createElement('a');
+
+//Add classes
+link.className = 'delete-item secondary-content';
+
+//Add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+
+//Append link into li
+li.appendChild(link);
+
+// Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+
+console.log(li);
+
+
+
 
