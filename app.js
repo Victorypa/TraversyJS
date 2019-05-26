f060e081df9a4a0b5bf2aca60267f039001bb5f3
Victorypa <victorypa1992@yandex.ru>
2019-05-26 18:07:16 +0300

    prototypal inheritance

--- app.js
+++ app.js
@@ -1258,34 +1258,73 @@ function Person(firstName, lastName, dob) {
 }
 
 // Calculate age
-Person.prototype.calculateAge = function(){
-  const diff =  Date.now() - this.birthday.getTime();
-  const ageDate = new Date(diff);
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
+// Person.prototype.calculateAge = function(){
+//   const diff =  Date.now() - this.birthday.getTime();
+//   const ageDate = new Date(diff);
+//   return Math.abs(ageDate.getUTCFullYear() - 1970);
+// }
+
+// // Get full name
+// Person.prototype.getFullName = function(){
+//   return `${this.firstName} ${this.lastName}`;
+// }
+
+// // Gets Married
+// Person.prototype.getsMaried = function(newLastName){
+//   this.lastName = newLastName;
+// }
+
+// const john = new Person('John', 'Doe', '8-12-90');
+// const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+
+// console.log(mary);
+
+// console.log(john.calculateAge());
+
+// console.log(mary.getFullName());
+
+// mary.getsMaried('Smith');
+
+// console.log(mary.getFullName());
+
+// console.log(mary.hasOwnProperty('firstName'));
+// console.log(mary.hasOwnProperty('getFullName'));
+
+
+//Person constructor
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
 }
 
-// Get full name
-Person.prototype.getFullName = function(){
-  return `${this.firstName} ${this.lastName}`;
+//Greeting
+Person.prototype.greeting = function() {
+  return `Hello there ${this.firstName} ${this.lastName}`
 }
 
-// Gets Married
-Person.prototype.getsMaried = function(newLastName){
-  this.lastName = newLastName;
+const person1 = new Person('John', 'Doe');
+
+// console.log(person1.greeting());
+
+// Costumer constructor
+function Customer(firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
+   
+  this.phone = phone;
+  this.membership = membership;
 }
+// Inherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
 
-const john = new Person('John', 'Doe', '8-12-90');
-const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+// Make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
 
-console.log(mary);
+// Create customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+console.log(customer1);
 
-console.log(john.calculateAge());
-
-console.log(mary.getFullName());
-
-mary.getsMaried('Smith');
-
-console.log(mary.getFullName());
-
-console.log(mary.hasOwnProperty('firstName'));
-console.log(mary.hasOwnProperty('getFullName'));
+// Customer greeting
+Customer.prototype.greeting = function(){
+  return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
+}
+console.log(customer1.greeting());
