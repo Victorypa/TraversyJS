f16bf50de8b024a25a5d38d9c8bb79afe0cae206
Victorypa <victorypa1992@yandex.ru>
2019-05-27 13:42:19 +0300

    sub classes

--- app.js
+++ app.js
@@ -1361,34 +1361,61 @@ const c = 3;
 
 
 // ES 6 classes
+// class Person {
+//   constructor(firstName, lastName, dob) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.birthday = new Date(dob);
+
+//   }
+//   greeting() {
+//     return `Hello there ${this.firstName} ${this.lastName}`;
+//   }
+//   calculateAge() {
+//     const diff = Date.now() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//   }
+//   getsMarried(newLastName) {
+//     this.lastName = newLastName;
+//   }
+
+//   static addNumbers(x, y) {
+//     return x + y;
+//   }
+// }
+
+// const mary = new Person('Mary', 'Williams', '11-13-1992');
+
+// mary.getsMarried('Thompson');
+// console.log(mary);
+// console.log(Person.addNumbers(1,2));
+
 class Person {
-  constructor(firstName, lastName, dob) {
+  constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.birthday = new Date(dob);
-
   }
   greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
   }
-  calculateAge() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
-  getsMarried(newLastName) {
-    this.lastName = newLastName;
+}
+
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName,);
+    this.phone = phone;
+    this.membership = membership;  
   }
 
-  static addNumbers(x, y) {
-    return x + y;
+  static getMembershipCost() {
+    return 500;
   }
 }
 
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
+
+console.log(john.greeting());
 
 
-const mary = new Person('Mary', 'Williams', '11-13-1992');
-
-mary.getsMarried('Thompson');
-console.log(mary);
-console.log(Person.addNumbers(1,2));
+console.log(Customer.getMembershipCost());
