34bb6ee44455937f0f1ec5c1d603252daf125a02
Victorypa <victorypa1992@yandex.ru>
2019-05-30 13:53:15 +0300

    callback functions

--- app.js
+++ app.js
@@ -1549,34 +1549,86 @@ const c = 3;
 
 
 // Chuck Norris Joke Generator
-document.querySelector('.get-jokes').addEventListener('click', getJokes);
+// document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
-function getJokes(e) {
-  const number = document.querySelector('input[type="number"]').value;
+// function getJokes(e) {
+//   const number = document.querySelector('input[type="number"]').value;
 
-  const xhr = new XMLHttpRequest();
+//   const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+//   xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
-  xhr.onload = function() {
-    if(this.status === 200) {
-      const response = JSON.parse(this.responseText);
+//   xhr.onload = function() {
+//     if(this.status === 200) {
+//       const response = JSON.parse(this.responseText);
       
-      let output = '';
+//       let output = '';
 
-      if(response.type === 'success') {
-        response.value.forEach(function(joke){
-          output += `<li>${joke.joke}</li>`;
-        });
-      } else {
-        output += '<li>Something went wrong</li>';
-      }
+//       if(response.type === 'success') {
+//         response.value.forEach(function(joke){
+//           output += `<li>${joke.joke}</li>`;
+//         });
+//       } else {
+//         output += '<li>Something went wrong</li>';
+//       }
 
-      document.querySelector('.jokes').innerHTML = output;
-    }
-  }
+//       document.querySelector('.jokes').innerHTML = output;
+//     }
+//   }
 
-  xhr.send();
+//   xhr.send();
 
-  e.preventDefault();
+//   e.preventDefault();
+// }
+
+
+// Callback functions
+
+
+// const posts = [
+//   {title: 'Post One', body: 'This is post one'},
+//   {title: 'Post Two', body: 'This is post two'}
+// ];
+
+
+// function createPost() {
+//   setTimeout(function() {
+//     posts.push(post);
+//   }, 2000); 
+// }
+
+
+// function getPosts() {
+//   setTimeout(function() {
+//     let output = '';
+//     posts.forEach(function(post) {
+//       output += `<li>${post.title}</li>`
+//     });
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
+
+// createPost({title: 'Post Three', body: 'This is post three'});
+
+// getPosts();
+
+
+function createPost(post, callback) {
+  setTimeout(function() {
+    posts.push(post);
+    callback();
+  }, 2000); 
 }
+
+
+function getPosts() {
+  setTimeout(function() {
+    let output = '';
+    posts.forEach(function(post) {
+      output += `<li>${post.title}</li>`
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
