125ad845859c33468f59cc68681bab3459cd949e
Victorypa <victorypa1992@yandex.ru>
2019-06-02 01:01:07 +0300

    The Fetch API

--- app.js
+++ app.js
@@ -1636,35 +1636,103 @@ const posts = [
 
 // ES6 Promises
 
-function createPost(post) {
+// function createPost(post) {
 
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      posts.push(post);
-      const error = true;
-      if(!error) {
-        resolve();
-      } else {
-        reject('Something went wrong')
-      } 
-    }, 2000); 
-  });
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       posts.push(post);
+//       const error = true;
+//       if(!error) {
+//         resolve();
+//       } else {
+//         reject('Something went wrong')
+//       } 
+//     }, 2000); 
+//   });
 
+// }
+
+
+// function getPosts() {
+//   setTimeout(function() {
+//     let output = '';
+//     posts.forEach(function(post) {
+//       output += `<li>${post.title}</li>`
+//     });
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
+
+// createPost({title: 'Post Three', body: 'This is post three'})
+// .then(getPosts)
+// .catch(function(err){
+// console.log(err);
+// });
+
+
+// THE FETCH API
+
+document.getElementById('button1').addEventListener('click', getText);
+
+document.getElementById('button2').addEventListener('click', getJson);
+
+document.getElementById('button3').addEventListener('click', getExternal);
+
+
+// Get local text file data
+function getText() {
+  fetch('test.txt')
+  .then(function(res){
+    return res.text();
+  })
+  .then(function(data){
+    console.log(data);
+    document.getElementById('output').innerHTML = data;
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+}
+
+// Get local JSON data
+
+// Get local json data
+function getJson() {
+  fetch('posts.json')
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post){
+        output += `<li>${post.title}</li>`
+      });
+      document.getElementById('output').innerHTML = output;
+  })
+  .catch(function(err){
+    console.log(err)
+  })
 }
 
 
-function getPosts() {
-  setTimeout(function() {
-    let output = '';
-    posts.forEach(function(post) {
-      output += `<li>${post.title}</li>`
-    });
-    document.body.innerHTML = output;
-  }, 1000);
-}
+// Get from external
 
-createPost({title: 'Post Three', body: 'This is post three'})
-.then(getPosts)
-.catch(function(err){
-console.log(err);
-});
+// Get local json data
+function getExternal() {
+  fetch('https://api.github.com/users')
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(user){
+        output += `<li>${user.login}</li>`
+      });
+      document.getElementById('output').innerHTML = output;
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+}
